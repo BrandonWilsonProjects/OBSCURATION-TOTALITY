5dc06556d48b21cac959907b9cb05359648893aa
Brandon Wilson <bzwilson@ncsu.edi>
2024-05-08 23:13:13 -0400

    edited the code

--- calculator.py
+++ calculator.py
@@ -1,2 +1,25 @@
-print(hello brandon)
+import ephem
 
+def compute_obscuration_totality(latitude, longitude):
+    obs = ephem.Observer()
+    obs.lat = str(latitude)
+    obs.lon = str(longitude)
+
+    sun = ephem.Sun()
+
+    # Set the date to April 8th, 2024
+    obs.date = '2024/4/8'
+
+    # Compute the details of the eclipse
+    eclipse_details = obs.previous_transit(sun)
+
+    # Compute the obscuration
+    obscuration = 1.0 - 10 ** (-0.4 * eclipse_details.imag)
+
+    return obscuration
+
+latitude = 31.55191
+longitude = -84.53428
+
+obscuration_totality = compute_obscuration_totality(latitude, longitude)
+print("Obscuration of totality on April 8th, 2024:", obscuration_totality)
