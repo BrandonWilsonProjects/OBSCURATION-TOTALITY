cc254dfad2a01bf7a688f674005774e88367cc98
Brandon Wilson <bzwilson@ncsu.edi>
2024-05-13 23:14:41 -0400

    update to the algorithm

--- calculator.py
+++ calculator.py
@@ -1,35 +1,42 @@
-import ephem
+import pandas as pd
+import ephem as ep
+import math
 
-def compute_obscuration_totality(latitude, longitude, date_time):
-    obs = ephem.Observer()
-    obs.lat = str(latitude)
-    obs.lon = str(longitude)
+def obscuration_algorithm(latitude, longitude, date):
+    observer = ep.Observer()
+    observer.lat = str(latitude)
+    observer.long = str(longitude)
+    observer.date = date
+    
+    sun = ep.Sun(observer)
+    moon = ep.Moon(observer)
+    
+    # Angular size of the Moon and the Sun in degrees
+    angular_size_sun = math.degrees(sun.size) * 3600
+    angular_size_moon = math.degrees(moon.size) * 3600
+    
+    # Calculating obsuration totality...
+    obscuration = (angular_size_sun - angular_size_moon) / angular_size_sun 
+    
+    return obscuration 
 
-    # Set the date and time
-    obs.date = date_time
+# Load data
+coordinates = pd.read_csv(r'C:\Users\bzwil\OneDrive\Documents\OBSCURATION ALGORITHM\OBSCURATION-TOTALITY\clean dataset - 4_8_24 total solar eclipse.csv')
 
-    # Compute the sun's position
-    sun = ephem.Sun(obs)
-    sun.compute(obs)
+# Date of Eclipse
+eclipse_date = "2024-04-08"
 
-    # Find the next solar eclipse
-    eclipse = ephem.(obs)
+# Calculating obscuration for each datapoint
+obscuration_data = []
+for index, row in coordinates.iterrows():
+    latitude = row['LATITUDE']
+    longitude = row['LONGITUDE']
+    obscuration = obscuration_algorithm(latitude, longitude, eclipse_date)
+    obscuration_data.append(obscuration)
+    
+# Adding obscuration totality to csv file
+coordinates['OBSCURATION'] = obscuration_data
 
-    # If there is no eclipse, return 0
-    if not eclipse:
-        return 0.0
+print(coordinates)
 
-    # Check if the time falls within the eclipse period
-    if eclipse[0] <= obs.date <= eclipse[4]:
-        # Compute the obscuration
-        obscuration = 1.0 - 10 ** (-0.4 * eclipse[6])
-        return obscuration
-
-    return 0.0
-
-latitude = 31.55191
-longitude = -84.53428
-date_time = '2024/4/8 19:00:00'
-
-obscuration_totality = compute_obscuration_totality(latitude, longitude, date_time)
-print("Obscuration of totality on April 8th, 2024 at 7:00 p.m.:", obscuration_totality)
+    
