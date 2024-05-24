b10008374b3b7a55a2137e9444b8b83f46ab5beb
Brandon Wilson <bzwilson@ncsu.edi>
2024-05-24 18:47:06 -0400

    adding the zipcode file to the folder. Excluding the zipcode package. Seems like it only reads strings... the data is in 'float64'

---
+++ preciseLocation.py
@@ -0,0 +1,27 @@
+# This file identifies the proximal location of coordinate points in a csv file.
+import pandas as pd
+import openpyxl
+import geopy as gp
+from geopy.geocoders import Nominatim
+from tqdm import tqdm
+
+
+# loading the cleaned latitude and longitude data and convering it to a data frame 
+location_data = pd.read_csv(r"C:\Users\bzwil\OneDrive\Desktop\OBSCURATION ALGORITHM\OBSCURATION-TOTALITY\Solar Eclipse Safari_CLEANED (ZIP CODE).csv")
+location_data_df = pd.DataFrame(location_data)
+
+# activating nominatim class...
+geolocator = Nominatim(user_agent="geocoding")
+
+# function for retrieving location data for each latitude and longitude row in the data frame
+def retrieving_location(row):
+    location = geolocator.geocode((row['ZIPCODE']), language='en')
+    return location.__getstate__
+
+# adding progress bars to the iteration 
+tqdm.pandas()
+location_data_df['LOCATION'] = location_data_df.progress_apply(retrieving_location, axis=1)
+
+# creating a new csv file with updated location data
+location_data_df.to_excel('new dataframe2.xlsx', index=False)
+print(location_data_df)
